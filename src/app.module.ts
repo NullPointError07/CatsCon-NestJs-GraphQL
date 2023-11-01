e28d80e335ae2c07c81ffdf4d7b06a8e9d02d772
NullPointError07 <md.taukir.alam@g.bracu.ac.bd>
2023-11-01 18:04:30 +0600

    project initialized

---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { AppResolver } from './app.resolver';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        console.log('ENV VAR', configService.get('MONGO_URI'));
+
+        const options: MongooseModuleOptions = {
+          uri: configService.get<string>('MONGO_URI'),
+        };
+        return options;
+      },
+    }),
+    ConfigModule.forRoot({
+      cache: true,
+    }),
+  ],
+  controllers: [],
+  providers: [AppService, AppResolver],
+})
+export class AppModule {}
